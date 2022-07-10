0b9e34414e6f50589e6f896632b44997152b90a0
mahendra patekar <mahendrapatekar09@gmail.com>
2022-07-11 00:43:10 +0530

    navbar

--- Project1/my-project/src/App.js
+++ Project1/my-project/src/App.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './component/Navbar';
+
 
 
 
@@ -8,41 +10,6 @@ function App() {
     <div className="App"> 
 
     
-    <div class="pt-1 mt-12 bg-gray-500 text-white hover:bg-green-200">
-  
-      <ul class="flex flex-row-reverse  space-x-4 space-x-reverse mx-10 ">
-        <button  class="flex">
-          
-      <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z" />
-</svg>
-<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z" />
-</svg>
-</button>
-        
- <div class="w-8 h-8 rounded hover:bg-indigo-400  ">
-   
- <img  src="logo192.png "></img>
-
-
- </div>
-
-        <li >
-          <a class="hover:text-purple-900 " >EMAIL</a>
-          </li> 
-          <li >
-          <a class="hover:text-purple-900 ">CONTACT</a>
-          </li> 
-          <li >
-          <a class="hover:text-purple-900 ">ABOUT</a>
-          </li> 
-          <li >
-          <a class="hover:text-purple-900">HOME</a>
-          </li> 
-        </ul> 
-   
-    </div>
     
 <button class="  py-2 px-4 mt-4 font-semibold rounded-full  shadow-md text-white bg-green-500 hover:bg-green-700  hover:text-black focus:outline-none focus:ring focus:ring-offset-2 hover:translate-y-3 transform transition focus:ring-green-700 focus:ring-opacity-50   ">
   Click me
@@ -75,11 +42,46 @@ SUBMIT
   </div>
 </div>
 </div>
-
-
+<div class="w-md mx-auto dark:bg-gray-700 mt-7 my-auto bg-gray-300 rounded-xl shadow-md overflow-hidden md:max-w-2xl">
+  <div class="md:flex ">
+    <div class="md:flex-shrink-0">
+      <img class="h-48 w-full object-cover md:h-full md:w-48 sm:h-48 " src="1.jpeg" alt="Man looking at item at a store"></img>
+    </div>
+    <div class="p-8">
+      <div class="uppercase tracking-wide text-sm   text-indigo-500 font-semibold dark:text-green-500">Case study</div>
+      <a href="#" class="block mt-1 text-lg leading-tight font-medium text-black hover:underline">Finding customers for your new business</a>
+      <p class="mt-2 text-gray-500  dark:bg-gray-500 dark:text-white">Getting a new business off the ground is a lot of hard work. Here are five ideas you can use to find your first customers.</p>
+    </div>
+  </div>
+</div><div class="w-md mx-auto dark:bg-gray-700 mt-7 my-auto bg-gray-300 rounded-xl shadow-md overflow-hidden md:max-w-2xl">
+  <div class="md:flex ">
+    <div class="md:flex-shrink-0">
+      <img class="h-48 w-full object-cover md:h-full md:w-48 sm:h-48 " src="1.jpeg" alt="Man looking at item at a store"></img>
+    </div>
+    <div class="p-8">
+      <div class="uppercase tracking-wide text-sm text-indigo-500 font-semibold dark:text-green-500">Case study</div>
+      <a href="#" class="block mt-1 text-lg leading-tight font-medium text-black hover:underline">Finding customers for your new business</a>
+      <p class="mt-2 text-gray-500  dark:bg-gray-500 dark:text-white">Getting a new business off the ground is a lot of hard work. Here are five ideas you can use to find your first customers.</p>
+    </div>
+  </div>
+</div><div class="w-md mx-auto dark:bg-gray-700 mt-7 my-auto bg-gray-300 rounded-xl shadow-md overflow-hidden md:max-w-2xl">
+  <div class="md:flex ">
+    <div class="md:flex-shrink-0">
+      <img class="h-48 w-full object-cover md:h-full md:w-48 sm:h-48 " src="1.jpeg" alt="Man looking at item at a store"></img>
+    </div>
+    <div class="p-8">
+      <div class="uppercase tracking-wide text-sm text-indigo-500 font-semibold dark:text-green-500">Case study</div>
+      <a href="#" class="block mt-1 text-lg leading-tight font-medium text-black hover:underline">Finding customers for your new business</a>
+      <p class="mt-2 text-gray-500  dark:bg-gray-500 dark:text-white">Getting a new business off the ground is a lot of hard work. Here are five ideas you can use to find your first customers.</p>
+    </div>
+  </div>
 </div>
 
-  );
+</div>
+>
+
+<Navbar></Navbar>
+ );
 }
 
 export default App;
