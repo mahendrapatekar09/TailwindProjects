3e5cf4eb5429660c9e2bda56b3ddf9543557e0b6
mahendra patekar <mahendrapatekar09@gmail.com>
2022-07-11 22:55:29 +0530

    text form js

---
+++ Project1/my-project/src/component/Textform.js
@@ -0,0 +1,29 @@
+import React ,{useState} from 'react';
+
+
+export default function Textform
+    () {
+        const handleupclick =() =>{
+            console.log("uppercase was clicked");
+            setText('you have clicked on handleupclick ')
+        }
+        const handleOnChange=() =>
+        {
+            console.log("on change");
+        }
+        const [text, setText] = useState('enter text here');
+  return (
+    <div>
+<h1> Enter text</h1>
+              <label for="message" class="block mt-8 mx-16 mb-2 text-sm font-medium text-gray-900 dark:text-gray-400" >Your message</label>
+<textarea id="message" rows="4" class=" p-2.5 w-96 mx-8 alig h-28 text-sm text-gray-900 bg
+-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500
+ dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white
+  dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Your message..." value={text} onChange={handleOnChange}></textarea>
+   <div>
+   <button className='bg-blue-300 ml-8 rounded text-white border-solid border-2 border-indigo-600 hover:bg-indigo-900'onClickCapture={handleupclick}>convert to upercase</button>
+   </div>
+    </div>
+    
+  )
+}
